2d2ce42e6b89fa1b1a900126ef0d6d368c6d6ff1
Tal Yo <tal@analink.com>
2022-09-20 08:31:37 -0400

    Added Bootstrap to index.html

--- src/app/operations/air-ams/air-ams-dashboard/air-ams-dashboard.component.ts
+++ src/app/operations/air-ams/air-ams-dashboard/air-ams-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-air-ams-dashboard',
@@ -7,7 +8,114 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirAmsDashboardComponent implements OnInit {
   date1 = new Date();
-  constructor() {}
+
+  selected!: string;
+
+  amsForm = this.fb.group({
+    bl: this.fb.group({
+      issuer: [''],
+      blType: [''],
+      bookingNo: [''],
+      carrierMasterBl: [''],
+      hBl: [''],
+      mib: [''],
+    }),
+
+    vessel: this.fb.group({
+      vType: [''],
+      carrierCode: [''],
+      flag: [''],
+      vName: [''],
+      voyage: [''],
+      vessCode: [''],
+    }),
+
+    ports: this.fb.group({
+      por: [''],
+      pol: [''],
+      etd: [''],
+      firstForPort: [''],
+      lastForPort: [''],
+      pou: [''],
+      eta: [''],
+      finalDest: [''],
+    }),
+
+    weights: this.fb.group({
+      quantity: [''],
+      uom: [''],
+      grossWt: [''],
+      unitWt: [''],
+      volume: [''],
+      unitVol: [''],
+    }),
+
+    container: this.fb.array([
+      {
+        contNo: [''],
+        contType: [''],
+        sealNo: [''],
+        loadStatus: [''],
+        service: [''],
+      },
+    ]),
+
+    relatedParties: this.fb.group({
+      shipper: this.fb.group({
+        shipperCode: [''],
+        name: [''],
+        address: [''],
+        telNo: [''],
+      }),
+      consignee: this.fb.group({
+        consigneeCode: [''],
+        name: [''],
+        address: [''],
+        telNo: [''],
+      }),
+      notify: this.fb.group({
+        notifyCode: [''],
+        name: [''],
+        address: [''],
+        telNo: [''],
+      }),
+      secondNotify: this.fb.group({
+        secondNotifyCode: [''],
+        name: [''],
+        address: [''],
+        telNo: [''],
+      }),
+    }),
+
+    amend: this.fb.group({
+      amendFlag: [''],
+      reason: [''],
+    }),
+  });
+
+  amendReason!: string;
+  amends: string[] = [
+    'New entry',
+    'Add a bill of lading',
+    'Delte a bill of lading',
+    'Replace manifest quantiy',
+  ];
+
+  reasons: string[] = [
+    '01 - Not laden aboard per evidence from foreign shipper, or amnended bill of lading',
+    '03 - Error in manifesting, not laden on this carrier; laden on subsequent carrier for transportation to United States, per evidence in files',
+    '04 - Clerical error in manifesting per bill of lading in files',
+    '05 - Pilfered or prematurely landed prior to arrival in United States per signed statement of master or his agent or vessel log extract in our file',
+  ];
+  constructor(private fb: UntypedFormBuilder) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    window.alert('Thank you for submitting Sea AMS data');
+  }
+
+  clearForm(): void {
+    this.amsForm.reset();
+  }
 }
--- src/app/operations/air-ams/air-ams.module.ts
+++ src/app/operations/air-ams/air-ams.module.ts
@@ -6,7 +6,10 @@ import { AirAmsNewComponent } from './air-ams-new/air-ams-new.component';
 import { AirAmsRoutingModule } from './air-ams-routing.module';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,13 @@ import { NgModule } from '@angular/core';
     AirAmsDetailComponent,
     AirAmsNewComponent,
   ],
-  imports: [CommonModule, MatCardModule, AirAmsRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    AirAmsRoutingModule,
+    MatFormFieldModule,
+    MatRadioModule,
+  ],
 })
 export class AirAmsModule {}
