f9af08f7f689531ffa0dda3cef7344d7314f22b7
taliho2021 <tal@analink.com>
2022-09-20 10:46:02 -0400

    updated layouts

---
+++ src/app/operations/air-ams/air-ams-grid/air-ams-grid.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AirAmsGridComponent } from './air-ams-grid.component';
+
+describe('AirAmsGridComponent', () => {
+  let component: AirAmsGridComponent;
+  let fixture: ComponentFixture<AirAmsGridComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AirAmsGridComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AirAmsGridComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/operations/air-ams/air-ams.module.ts
+++ src/app/operations/air-ams/air-ams.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AirAmsRoutingModule } from './air-ams-routing.module';
 import { AirAmsComponent } from './air-ams.component';
+import { AirAmsDashboardComponent } from './air-ams-dashboard/air-ams-dashboard.component';
+import { AirAmsGridComponent } from './air-ams-grid/air-ams-grid.component';
+
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
-    AirAmsComponent
+    AirAmsComponent,
+    AirAmsDashboardComponent,
+    AirAmsGridComponent
   ],
   imports: [
     CommonModule,
-    AirAmsRoutingModule
+    MatCardModule,
+    AirAmsRoutingModule,
   ]
 })
 export class AirAmsModule { }
